5403461f0c69496777ec2a7bd0c15a004bcd88c8
Young Liu <youngliu92@gmail.com>
2019-04-25 16:45:02 -0700

    add callback for data flow.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import Header from './Header';
 import TicketList from './TicketList';
-import NewTicketForm from './NewTicketForm';
+import NewTicketControl from './NewTicketControl';
 import Error404 from './Error404';
 import { Switch, Route } from 'react-router-dom';
 
-function App(){
+function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <Switch>
         <Route exact path='/' component={TicketList} />
-        <Route path='/newticket' component={NewTicketForm} />
+        <Route path='/newticket' component={NewTicketControl} />
         <Route component={Error404} />
       </Switch>
     </div>
